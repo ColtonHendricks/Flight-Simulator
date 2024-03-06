b24efd2712ad01f78058563e1f49ff79cb8dc531
ColtonHendricks <coleminer1758@gmail.com>
2024-03-06 17:06:01 -0500

    Added Pitch, Yaw, and Roll

---
+++ Assets/PlaneController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+	public float throttleIncrement = 0.1f;
+	public float maxThrust = 200f;
+	public float responsiveness = 10f;
+
+	private float throttle;
+	private float roll;
+	private float yaw;
+	private float pitch;
+
+	private float responseModifier {
+		get {
+			return(rb.mass/10f) * responsiveness;
+		}
+	}
+	Rigidbody rb;
+
+	private void Awake(){
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void HandleInputs() {
+		roll = Input.GetAxis("Roll");
+		pitch = Input.GetAxis("Pitch");
+		yaw = Input.GetAxis("Yaw");
+
+		if(Input.GetKey(KeyCode.Space)) throttle += throttleIncrement;
+		else if (Input.GetKey(KeyCode.LeftControl)) throttle -= throttleIncrement;
+		throttle = Mathf.Clamp(throttle, 0f, 100f);
+	}
+
+	private void Update() {
+		
+		HandleInputs();
+	}
+
+	private void FixedUpdate() {
+
+		rb.AddForce(transform.forward * maxThrust * throttle);
+		rb.AddTorque(transform.up * yaw * responseModifier);
+		rb.AddTorque(transform.forward * pitch * responseModifier);
+		rb.AddTorque(transform.right * roll * responseModifier);
+
+	}
+}
