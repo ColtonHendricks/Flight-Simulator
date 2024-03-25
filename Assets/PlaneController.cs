2021cc4e70a65917fb9c1d2fc98cbd58fc03f0b0
ColtonHendricks <coleminer1758@gmail.com>
2024-03-24 23:41:33 -0400

    Updated Lots of things - Details in Description
    Added basic hud
    Added flight physics and flight controls
    Added new camera controller
    Added spinning propellor
    Deleted old camera controller

--- Assets/PlaneController.cs
+++ Assets/PlaneController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlaneController : MonoBehaviour
 {
 	public float throttleIncrement = 0.1f;
 	public float maxThrust = 200f;
 	public float responsiveness = 10f;
+	public float lift = 135f;
 
 	private float throttle;
 	private float roll;
@@ -19,6 +21,8 @@ public class PlaneController : MonoBehaviour
 		}
 	}
 	Rigidbody rb;
+	[SerializeField] TextMeshProUGUI hud; 
+	[SerializeField] Transform propellor;
 
 	private void Awake(){
 		rb = GetComponent<Rigidbody>();
@@ -37,14 +41,23 @@ public class PlaneController : MonoBehaviour
 	private void Update() {
 		
 		HandleInputs();
+		UpdateHUD();
+
+		propellor.Rotate(Vector3.right * throttle);
 	}
 
 	private void FixedUpdate() {
 
-		rb.AddForce(transform.forward * maxThrust * throttle);
+		rb.AddForce(-transform.right * maxThrust * throttle);
 		rb.AddTorque(transform.up * yaw * responseModifier);
 		rb.AddTorque(transform.forward * pitch * responseModifier);
 		rb.AddTorque(transform.right * roll * responseModifier);
+		rb.AddForce(transform.up * rb.velocity.magnitude * lift);
 
+	}
+	private void UpdateHUD() {
+		hud.text = "Throttle: " + throttle.ToString("F0") + "%\n";
+		hud.text += "Airspeed: " + (rb.velocity.magnitude * 1.9f).ToString("F0") + " knots \n";
+		hud.text += "Altitude: " + transform.position.y.ToString("F0") + " m";
 	}
 }
